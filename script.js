8adbabd5351afbcfcc2e085393f55444641cec4d
TableHammer <awelebenedict1@gmail.com>
2025-05-13 02:37:59 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const
+document.addEventListener('DOMContentLoaded', function() {
+    const questContainer = document.getElementById('questContainer');
+    const questDetails = document.getElementById('detailsContent');
+    const completeBtn = document.getElementById('completeBtn');
+    const addQuestBtn = document.getElementById('addQuestBtn');
+    const removeQuestBtn = document.getElementById('removeQuestBtn');
+})
